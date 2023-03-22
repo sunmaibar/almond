b8105c97f56746935131d514a961b4802197eda9
Jimmy <jajalaba@yahoo.com.tw>
2023-03-22 19:06:39 +0800

    dwf

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,16 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import { Layout, Hero, About, Projects, Contact, Howto } from '../components'
-import { GatsbtProvider } from '../context/context'
 
 const HomePage = () => {
   return (
-    <GatsbtProvider>
-      <Layout>
-        <Hero />
-        <About />
-        <Projects />
-        <Howto />
-        <Contact />
-      </Layout>
-    </GatsbtProvider>
+    <Layout>
+      <Hero />
+      <About />
+      <Projects />
+      <Howto />
+      <Contact />
+    </Layout>
   )
 }
 
